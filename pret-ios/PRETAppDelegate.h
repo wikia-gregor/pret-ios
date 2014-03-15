43d3af92d1184b8a5b8787f58ef0fc14aa2d6c1d
Grzegorz Nowicki <grzegorz@wikia-inc.com>
2014-03-15 12:55:54 +0100

    Initial project commit

---
+++ pret-ios/PRETAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRETAppDelegate.h
+//  PRET
+//
+//  Created by Gregor on 15.03.2014.
+//  Copyright (c) 2014 Wikia Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRETAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
